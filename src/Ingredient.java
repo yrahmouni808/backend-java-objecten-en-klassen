4d1f1c4f6b69fac10d3a450659fc3495b619b352
yrahmouni808 <neonidas2004@gmail.com>
2025-03-26 14:17:18 +0100

    added some substance

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -1,3 +1,13 @@
+package src;
+
 public class Ingredient {
-    private
+    private String name;
+    private int amount;
+    private String unit;
+
+    public Ingredient(String name, int amount, String unit){
+        this.amount = amount;
+        this.name = name;
+        this.unit = unit;
+    }
 }
