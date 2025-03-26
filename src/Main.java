2727b5890fe837575e9f99634e8b0ddf8d405622
yrahmouni808 <neonidas2004@gmail.com>
2025-03-26 15:24:09 +0100

    In Main toegepast

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-
+        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
+        applePieRecipe.printIngredients();
+        applePieRecipe.printSteps();
     }
 }
