3ae39d8ac3acb65dcb9ef6caff5d757988c905ac
yrahmouni808 <neonidas2004@gmail.com>
2025-03-26 16:01:44 +0100

    Bonusopdracht 2 gemaakt.

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,13 @@ package src;
 
 public class Main {
     public static void main(String[] args) {
-        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
-        applePieRecipe.printIngredients();
-        applePieRecipe.printSteps();
+//        ApplePieRecipe applePieRecipe = new ApplePieRecipe();
+//        applePieRecipe.printIngredients();
+//        applePieRecipe.printSteps();
+        Recipe brownie = new Recipe();
+        brownie.addIngredient("Chocolate", 100, "gram");
+        brownie.printIngredients();
+        brownie.addStep("Throw it at the wall.");
+        brownie.printSteps();
     }
 }
