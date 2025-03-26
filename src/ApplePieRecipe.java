803785c3f9c7047de98112ac1a7cb22da4a3174e
yrahmouni808 <neonidas2004@gmail.com>
2025-03-26 14:36:41 +0100

    Ben begonnen met de stappen in ApplePieRecipe.java

--- src/ApplePieRecipe.java
+++ src/ApplePieRecipe.java
@@ -1,4 +1,67 @@
 package src;
 
 public class ApplePieRecipe {
+    public ApplePieRecipe(){
+
+    }
+    Ingredient roomBoter = new Ingredient(
+            "ongezouten roomboter",
+            200,
+            "gram"
+    );
+    Ingredient basterdSuiker = new Ingredient(
+            "witte bastard suiker",
+            200,
+            "gram"
+    );
+    Ingredient bakMeel = new Ingredient(
+            "zelfrijzend bakmeel",
+            400,
+            "gram"
+    );
+    Ingredient ei = new Ingredient(
+            "ei",
+            1,
+            "stuk"
+    );
+    Ingredient vanilleSuiker = new Ingredient(
+            "vanillesuiker",
+            8,
+            "gram"
+    );
+    Ingredient zout = new Ingredient(
+            "zout",
+            1,
+            "snuf"
+    );
+    Ingredient appel = new Ingredient(
+            "zoetzure appels",
+            1.5f,
+            "kilo"
+    );
+    Ingredient kristalSuiker = new Ingredient(
+            "kristal suiker",
+            75,
+            "gram"
+    );
+    Ingredient kaneel = new Ingredient(
+            "kaneel",
+            3,
+            "theelepels"
+    );
+    Ingredient paneerMeel = new Ingredient(
+            "paneermeel",
+            15,
+            "gram"
+    );
+    public void voorVerwarmen(){
+        System.out.println("Verwarm de oven van te voren op 170 graden Celsius (boven " +
+                "en onderwarmte)");
+    }
+    public void eiKloppen(){
+        System.out.println(
+                "Klop het " + ei.getName() + " los en verdeel deze in 2 delen. De ene helft"
+                + " voor het deeg"
+        );
+    }
 }
--- src/Ingredient.java
+++ src/Ingredient.java
@@ -2,12 +2,36 @@ package src;
 
 public class Ingredient {
     private String name;
-    private int amount;
+    private float amount;
     private String unit;
 
-    public Ingredient(String name, int amount, String unit){
+    public Ingredient(String name, float amount, String unit){
         this.amount = amount;
         this.name = name;
+        this.unit = unit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
         this.unit = unit;
     }
 }
