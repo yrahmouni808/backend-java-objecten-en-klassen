b7162d7b982a3b4be5b16fd4b279269c813cb1cd
yrahmouni808 <neonidas2004@gmail.com>
2025-03-28 11:20:09 +0100

    Store bonusopracht gedaan

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 package src;
 
 import Bonus.Film;
+import Bonus.Product;
+import Bonus.Store;
 
 import java.time.LocalDate;
 
@@ -16,5 +18,11 @@ public class Main {
         brownie.printSteps();
         Film idk = new Film("Robert-Jan", "Rowan", LocalDate.of(2025, 3, 26), "Adult");
         idk.printInfo();
+        Store winkel = new Store();
+        winkel.addProduct(new Product("telefoon", 699.99), 500);
+        winkel.printInfo();
+        winkel.removeItem("telefoon", 200);
+        winkel.printInfo();
+        winkel.removeItem("telefoon", 0);
     }
 }
